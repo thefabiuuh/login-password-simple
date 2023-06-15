adaab46dd0831a641dc0a7ec96dfea91fe21ed59
thefabiuuh <fabiocontas78@gmail.com>
2023-06-15 16:57:07 -0300

    fisrt commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function logar() {
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin" && senha == "admin" ) {
+        alert('Logado com sucesso!');
+        location.href = "home.html";
+    }   
+    else{
+        alert('Usuario ou senha incorreto.')
+    }
+}
